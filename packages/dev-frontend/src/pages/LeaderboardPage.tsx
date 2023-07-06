0c552b1a06610ad7187aa28b1ec86272ac52c3ba
Isaac Wooden <iwooden@protonmail.com>
2023-07-06 14:40:52 -0700

    fix doc link

--- packages/dev-frontend/src/pages/LeaderboardPage.tsx
+++ packages/dev-frontend/src/pages/LeaderboardPage.tsx
@@ -5,12 +5,12 @@ import { Leaderboard } from "../components/Leaderboard";
 
 export const LeaderboardPage = (): JSX.Element => {
   return <Container variant="singlePage">
-      <PageHeading
-        heading="Leaderboard"
-        descriptionTitle="Leaderboard for LED Tournament"
-        description="Top performing addresses for the LED tournament. Score is determined by assets (USD and LED) minus any outstanding LED debt."
-        link="https://github.com/Threshold-USD/dev"
-      />
-      <PageRow isWidthFull={true} Component={Leaderboard} />
+    <PageHeading
+      heading="Leaderboard"
+      descriptionTitle="Leaderboard for LED Tournament"
+      description="Top performing addresses for the LED tournament. Score is determined by assets (USD and LED) minus any outstanding LED debt."
+      link="https://codyborn.notion.site/LED-Trading-Tournament-Guide-6fb50c860dfb41ada89e9e67528140cc"
+    />
+    <PageRow isWidthFull={true} Component={Leaderboard} />
   </Container>
 };
